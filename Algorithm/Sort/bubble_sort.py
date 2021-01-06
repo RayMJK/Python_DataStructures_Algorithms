381c9b9991f632fc79bf9517f58f542a2377b5b2
RayMJK <mjkraymond93@gmail.com>
2021-01-07 00:26:45 +0900

    1/7

---
+++ Algorithm/Sort/bubble_sort.py
@@ -0,0 +1,16 @@
+import random
+
+def bubblesort(data):
+    for index in range(len(data) - 1):
+        swap = False
+        for index2 in range(len(data) - index - 1):
+            if data[index2] > data[index2 + 1]:
+                data[index2], data[index2 + 1] = data[index2 + 1], data[index2]
+                swap = True
+
+        if swap == False:
+            break
+    return data
+
+data_list = random.sample(range(100), 50)
+print (bubblesort(data_list))

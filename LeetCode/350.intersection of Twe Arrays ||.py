bd67f7bcf0a16079c95a23d6c8c83323292d9249
RayMJK <mjkraymond93@gmail.com>
2021-01-08 00:21:45 +0900

    sort

--- LeetCode/350.intersection of Twe Arrays ||.py
+++ LeetCode/350.intersection of Twe Arrays ||.py
@@ -1,29 +1,13 @@
-def intersect(nums1, nums2):
-    """
-    :type nums1: List[int]
-    :type nums2: List[int]
-    :rtype: List[int]
-    """
-    a = sorted(nums1)  # [1,1,2,2]
-    b = sorted(nums2)  # [2,2]
-    i = 0
-    result = []
-    while i < len(a):
-        j = 0
-        while j < len(b):
-            if a[i] == b[j]:
-                c = a[i]
-                b.pop(j)
-                result.append(c)
-            j += 1
-        i += 1
+class Solution:
+    def intersect(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        answer = []
 
+        for i in nums1:
+            if nums2:
+                for j in nums2:
+                    if i == j:
+                        answer.append(j)
+                        nums2.remove(j)
+                        break
 
-    return result
-
-nums1 = [4,9,5]
-nums2 = [9,4,9,8,4]
-
-print(intersect(nums1, nums2))
-
-
+        return answer

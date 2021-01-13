d9d2117ed1a7487806178849ebb4db8ea59c6042
RayMJK <mjkraymond93@gmail.com>
2021-01-13 21:24:51 +0900

    datastructures

---
+++ Data_Structures/Linked_List/LinkedList.py
@@ -0,0 +1,76 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self, value):
+        self.head = Node(value)
+
+    def append(self, value):
+        cur = self.head
+        while cur.next is not None:
+            cur = cur.next
+        cur.next = Node(value)
+
+    def print_all(self):
+        cur = self.head
+        while cur is not None:
+            print(cur.data)
+            cur = cur.next
+
+    def get_node(self, index):
+        node = self.head
+        count = 0
+        while count < index:
+            node = node.next
+            count += 1
+
+        return node
+
+    def add_node(self, index, value):
+        new_node = Node(value)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+            return
+        node = self.get_node(index-1)
+        next_node = node.next
+        node.next = new_node
+        new_node.next = next_node
+
+    def delete_node(self, index):
+        if index == 0:
+            self.head = self.head.next
+
+        node = self.get_node(index-1)
+        node.next = node.next.next
+        return
+
+def get_linked_list_sum(linked_list_1, linked_list_2):
+    sum_1 = _get_linked_list_sum(linked_list_1)
+    sum_2 = _get_linked_list_sum(linked_list_2)
+    print(sum_1)
+    print(sum_2)
+    return sum_1 + sum_2
+
+def _get_linked_list_sum(linked_list):
+    linked_list_sum = 0
+    head = linked_list.head
+
+    while head is not None:
+        linked_list_sum = linked_list_sum * 10 + head.data
+        head = head.next
+    return linked_list_sum
+
+# [6] -> [7] -> [8]
+linked_list_1 = LinkedList(6)
+linked_list_1.append(7)
+linked_list_1.append(8)
+
+# [3] -> [5] -> [4]
+linked_list_2 = LinkedList(3)
+linked_list_2.append(5)
+linked_list_2.append(4)
+
+print(get_linked_list_sum(linked_list_1, linked_list_2))

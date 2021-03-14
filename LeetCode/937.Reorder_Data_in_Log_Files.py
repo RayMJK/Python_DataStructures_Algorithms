2181bbc4764b4fb9d5c4884b34b9e2d34c22aadd
RayMJK <mjkraymond93@gmail.com>
2021-03-15 01:48:21 +0900

    3/15

---
+++ LeetCode/937.Reorder_Data_in_Log_Files.py
@@ -0,0 +1,6 @@
+class Solution(object):
+    def reorderLogFiles(self, logs):
+        """
+        :type logs: List[str]
+        :rtype: List[str]
+        """
--- LeetCode/prac.py
+++ LeetCode/prac.py
@@ -1,5 +1,39 @@
-a = {'d': 1, 'b': 20, 'c': 20}
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
 
 
-result = [k for k,v in a.items() if max(a.values()) == v]
-print(result)
+#
+# Complete the 'groupDivision' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY levels
+#  2. INTEGER maxSpread
+#
+
+def groupDivision(levels, maxSpread):
+    # Write your code here
+    answer = 1
+    stack = [levels[0]]
+    levels = sorted(levels)
+    if len(levels) == 0:
+        return 0
+    elif maxSpread == 0 and len(list(set(levels))) != 1:
+        return len(list(set(levels)))
+    else:
+        for i in range(1, len(levels)):
+            if levels[i] - stack[0] <= maxSpread:
+                stack.append(levels[i])
+            else:
+                new = levels[i]
+                stack = [new]
+                answer += 1
+    return answer
+
+
+if __name__ == '__main__':

3f232d680b08150052c458376ed3ed6235f5edec
RayMJK <mjkraymond93@gmail.com>
2021-03-19 23:14:51 +0900

    3.18

--- Programmers/문자열내마음대로정렬하기.py
+++ Programmers/문자열내마음대로정렬하기.py
@@ -1,12 +1,14 @@
 def solution(strings, n):
+
     answer = []
     result = dict()
     for i in strings:
         result[i] = i[n]
-    print(result)
     result = sorted(result.items(), key=lambda x:x[1])
     result = sorted(result, key = lambda x : (x[1], x[0]))
-    return result
+    for i in result:
+        answer.append(i[0])
+    return answer
 strings = ["abce", "abcd", "cdx"]
 n = 2
 print(solution(strings, n))

c224cf3b9a8cf35454daf6fdaf94e5e65687f029
RayMJK <mjkraymond93@gmail.com>
2021-03-29 00:25:56 +0900

    3.28

--- Programmers/이상한문자만들기.py
+++ Programmers/이상한문자만들기.py
@@ -1,17 +1,13 @@
 def solution(s):
     answer = ''
-    for i in range(len(s)):
-        if i % 2 == 0:
-            if s[i] == ' ':
-                answer += s[i]
+    result = s.split(' ')
+    for word in result:
+        for i in range(len(word)):
+            if i % 2 == 0:
+                answer += word[i].upper()
             else:
-                answer += s[i].upper()
-        else:
-            if s[i] == ' ':
-                answer += s[i]
-            else:
-                answer += s[i].lower()
+                answer += word[i].lower()
 
+        answer += ' '
+    answer = answer[:-1]
     return answer
-
-print(solution("try hello world"))

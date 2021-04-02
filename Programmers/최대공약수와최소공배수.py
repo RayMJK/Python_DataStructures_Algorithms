4526d2623991b5f58b316d3d91beee2a3b15f772
RayMJK <mjkraymond93@gmail.com>
2021-04-02 23:05:37 +0900

    4.2

--- Programmers/최대공약수와최소공배수.py
+++ Programmers/최대공약수와최소공배수.py
@@ -1,19 +1,17 @@
 def solution(n, m):
     answer = []
-    result = []
-    for i in range(1, min(n,m)+1):
-        if min(n,m)%i == 0:
-            if max(n,m) % i == 0:
-                answer.append(i)
-    if len(answer) == 1:
-        gcf = answer.pop()
-        result.append(gcf)
-        result.append(n*m)
-    else:
-        gcf = answer.pop()
-        lcm = max(n,m)
-        result.append(gcf)
-        result.append(lcm)
-    return result
+    if max(n, m) % min(n, m) == 0:
+        answer.append(min(n, m))
+        answer.append(max(n, m))
+        return answer
 
-print(solution(1000000, 999999))
+    for i in range(1, min(n, m) + 1):
+        if min(n, m) % i == 0:
+            if max(n, m) % i == 0:
+                gcf = i
+    lcm = min(n, m) * max(n, m) // gcf
+    answer.append(gcf)
+    answer.append(lcm)
+    return answer
+
+print(solution(21, 24))

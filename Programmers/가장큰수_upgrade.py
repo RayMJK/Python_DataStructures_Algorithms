74776811ae58aeb50bb4cce438a512ca8ab6b613
RayMJK <mjkraymond93@gmail.com>
2022-01-02 17:13:05 +0900

    new year

--- Programmers/가장큰수_upgrade.py
+++ Programmers/가장큰수_upgrade.py
@@ -3,7 +3,8 @@ def solution(numbers):
     numbers = list(map(str, numbers))
     # print(numbers)
     numbers.sort(key = lambda x:x*3, reverse=True)
-    # print(numbers)
+    #print(numbers)
 
     answer = str(int(''.join(numbers)))
     return answer
+

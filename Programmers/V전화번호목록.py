836832968dcaf619f92f84755a72a6b2e4562712
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-01-17 21:22:46 +0900

    1/17

--- Programmers/V전화번호목록.py
+++ Programmers/V전화번호목록.py
@@ -1,9 +1,8 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    if len(phone_book) == 1:
-        return answer
+    #print(phone_book)
     for i in range(len(phone_book)-1):
-        if phone_book[i] == phone_book[i+1][:len(phone_book[i])]:
+        if phone_book[i] in phone_book[i+1][:len(phone_book)]:
             return False
     return answer

7577307dab0139b7c58e44db9bce71ec70b5c3c8
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-01-17 18:02:42 +0900

    Update V3_완주하지못한선수.py

--- Programmers/V3_완주하지못한선수.py
+++ Programmers/V3_완주하지못한선수.py
@@ -3,10 +3,8 @@ def solution(participant, completion):
     answer = ''
     #print(collections.Counter(participant))
     #print(collections.Counter(completion))
-    answer = collections.Counter(participant) - collections.Counter(completion)
-
-
-    #print(answer)
-
     
+    answer = collections.Counter(participant) - collections.Counter(completion)
+    #print(answer)
+    #print(list(answer)[0])
     return list(answer)[0]

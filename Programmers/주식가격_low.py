9ccb967c25d7e1ec4d29af0e3740bde9e46174ba
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-01-20 16:25:59 +0900

    Update 주식가격_low.py

--- Programmers/주식가격_low.py
+++ Programmers/주식가격_low.py
@@ -6,18 +6,18 @@ def solution(prices):
             stack.pop()
         stack.append(prices[i])
         a = 0
-        cur = -1
+        flag = -1
         for j in range(i+1, len(prices)):
             #print(stack)
             if stack[0] > prices[j]:
-                cur = 0
+                flag = 0
                 #print(i, j-i)
                 answer.append(j-i)
                 stack.pop()
                 break
             else:
                 a+=1
-        if cur == -1:
+        if flag == -1:
             answer.append(a)
         #print(answer)
     answer.append(0)            

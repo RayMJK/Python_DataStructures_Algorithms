abd1290d6f23e84697503aacd0d802201d0118cc
RayMJK <mjkraymond93@gmail.com>
2022-01-22 14:49:21 +0900

    1.22

--- Programmers/V_더맵게.py
+++ Programmers/V_더맵게.py
@@ -1,13 +1,13 @@
 import heapq
-
 def solution(scoville, K):
     answer = 0
     heapq.heapify(scoville)
-    
+
     while scoville[0] < K:
-        mix = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
-        heapq.heappush(scoville, mix)
         answer += 1
+        scov = heapq.heappop(scoville) + (heapq.heappop(scoville) * 2)
+        heapq.heappush(scoville, scov)
+
         if len(scoville) == 1 and scoville[0] < K:
             return -1
     return answer

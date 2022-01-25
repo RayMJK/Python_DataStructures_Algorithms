6abfbdd8fadf9ebd4ad7661ce537d9b267896835
RayMJK <mjkraymond93@gmail.com>
2022-01-25 23:28:28 +0900

    1.25

--- Programmers/V_디스크컨트롤러.py
+++ Programmers/V_디스크컨트롤러.py
@@ -1,14 +1,18 @@
 import heapq
+
+
 def solution(jobs):
-    answer, now, i = 0, 0, 0
+    answer = 0
+    now = 0
+    i = 0
     start = -1
     heap = []
-    
+
     while i < len(jobs):
         for j in jobs:
             if start < j[0] <= now:
                 heapq.heappush(heap, [j[1], j[0]])
-        
+
         if len(heap) > 0:
             cur = heapq.heappop(heap)
             start = now
@@ -17,6 +21,5 @@ def solution(jobs):
             i += 1
         else:
             now += 1
-            
+
     return answer // len(jobs)
-    

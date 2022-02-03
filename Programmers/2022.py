dace342ce130eea6877a6792a88a0982dedd77ea
RayMJK <mjkraymond93@gmail.com>
2022-02-04 00:46:26 +0900

    2.3

--- Programmers/2022.py
+++ Programmers/2022.py
@@ -1,24 +1,3 @@
-import heapq
-
-
-def solution(jobs):
-    answer = 0
-    now = 0
-    i = 0
-    start = -1
-    heap = []
-
-    while i < len(jobs):
-        for j in jobs:
-            if start < j[0] <= now:
-                heapq.heappush(heap, [j[1], j[0]])
-        if len(heap) > 0:
-            current = heapq.heappop(heap)
-            start = now
-            now += current[0]
-            answer += now - current[1]
-            i += 1
-        else:
-            now += 1
-
-    return answer // len(jobs)
+def solution(operations):
+    answer = []
+    return answer

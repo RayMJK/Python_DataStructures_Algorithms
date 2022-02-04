a409ee2ca4d37cf77c2f8f259d3f142fc45efa6a
RayMJK <mjkraymond93@gmail.com>
2022-02-04 23:44:16 +0900

    2.4

--- Programmers/2022.py
+++ Programmers/2022.py
@@ -1,29 +1,31 @@
 def solution(answers):
     answer = []
-    first = [1, 2, 3, 4, 5]
-    second = [2, 1, 2, 3, 2, 4, 2, 5]
-    third = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
-    count_first = 0
-    count_second = 0
-    count_third = 0
+    #print(answers)
+    a1 = [1,2,3,4,5]
+    a2 = [2, 1, 2, 3, 2, 4, 2, 5]
+    a3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    a1_count = 0
+    a2_count = 0
+    a3_count = 0
     result = []
     for i in range(len(answers)):
-        if answers[i] == first[i % len(first)]:
-            count_first += 1
-
-        if answers[i] == second[i % len(second)]:
-            count_second += 1
-
-        if answers[i] == third[i % len(third)]:
-            count_third += 1
-
-    answer.append(count_first)
-    answer.append(count_second)
-    answer.append(count_third)
-    # print('answer =', answer)
-    max_score = max(answer)
-    for person, score in enumerate(answer):
-        if score == max_score:
-            result.append(person + 1)
-
-    return result
+        s1 = i % 5
+        s2 = i % 8
+        s3 = i % 10
+        if answers[s1] == a1[i]:
+            a1_count += 1
+        if answers[s2] == a2[i]:
+            a2_count += 1
+        if answers[s3] == a3[i]:
+            a3_count += 1
+    result.append(a1_count)
+    result.append(a2_count)
+    result.append(a2_count)
+    max_answer = max(result)
+    if a1_count == max_answer:
+        answer.append(1)
+    if a2_count == max_answer:
+        answer.append(2)
+    if a3_count == max_answer:
+        answer.append(3)
+    return answer

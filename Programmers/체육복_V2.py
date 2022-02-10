4034536b6a691ae1894fcfb2b6c7bf066ef775db
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-02-10 11:23:09 +0900

    Update 체육복_V2.py

--- Programmers/체육복_V2.py
+++ Programmers/체육복_V2.py
@@ -1,27 +1,23 @@
 def solution(n, lost, reserve):
     answer = []
+    for a in reserve:
+        if a in lost:
+            reserve.remove(a)
+            lost.remove(a)
     for i in range(1, n+1):
-        print('i = ', i)
-        if i in lost:
-            if i in reserve:
-                answer.append(i)
+        if i in reserve:
+            if i in lost:
                 reserve.remove(i)
-                break
-            else:
-                if i-1 in reserve:
-                    answer.append(i)
-                    reserve.remove(i-1)
-                    print('ssibal=',answer)
-                    break
-                elif i+1 in reserve:
-                    answer.append(i)
-                    reserve.remove(i+1)
-        else:
             answer.append(i)
-            print('sssibal =', answer)
-        print('reserve =', reserve)
-        print('answer =', answer)
-    #print(answer)
-    return len(list(set(answer)))
-
-print(solution(5, [2,4], [1,3,5]))
+        else:
+            if i in lost:
+                if i-1 in reserve:
+                    reserve.remove(i-1)
+                    answer.append(i)
+                elif i+1 in reserve:
+                    reserve.remove(i+1)
+                    answer.append(i)
+            else:
+                answer.append(i)
+                
+    return len(answer)

f8272c5bb203aae2edfe5430eab5a222c16c1c76
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-02-11 17:43:22 +0900

    2.11

---
+++ Programmers/섬연결하기.py
@@ -0,0 +1,11 @@
+def solution(n, costs):
+    answer = 0
+    print(costs)
+    stack = []
+    
+    for i in range(4):
+        a = []
+        for j in range(len(costs)):
+            if i == j[0]:
+                
+    return answer

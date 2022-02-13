eca5cf4a12209ffb3624b52aaac78f57064b0bb5
RayMJK <mjkraymond93@gmail.com>
2022-02-13 23:40:06 +0900

    2.13

--- Programmers/섬연결하기.py
+++ Programmers/섬연결하기.py
@@ -1,13 +1,18 @@
 def solution(n, costs):
     answer = 0
-    print(costs)
     stack = []
-    
-    for i in range(4):
-        a = []
+    for i in range(n):
+        cost = dict()
         for j in range(len(costs)):
-            if i == j[0]:
-
-                
-                
+            if i == costs[j][0]:
+                stack.append(i)
+                cost[costs[j][1]] = costs[j][2]
+        min_val = min(cost.values())
+        min_key = min(cost, key=cost.get)
+        stack.append(min_key)
+        answer += min_val
+    stack = list(set(stack))
+    print(stack)
     return answer
+
+print(solution(4, [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]	))

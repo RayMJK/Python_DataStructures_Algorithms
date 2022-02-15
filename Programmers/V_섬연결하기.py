25ab7da5ad3780874c3d971e466aab0c5afb16e2
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-02-15 10:46:53 +0900

    Update V_섬연결하기.py

--- Programmers/V_섬연결하기.py
+++ Programmers/V_섬연결하기.py
@@ -1,18 +1,16 @@
 def solution(n, costs):
     answer = 0
-    # print(costs)
+
     costs.sort(key=lambda x: x[2])
-    #print(costs)
-    connect = set([costs[0][0]])
-    #print('connect =', connect)
+    routes = set([costs[0][0]])
 
-    while len(connect) != n:
-        for cost in costs:
-            if cost[0] in connect and cost[1] in connect:
+    while len(routes) != n:
+        for i, cost in enumerate(costs):
+            if cost[0] in routes and cost[1] in routes:
                 continue
-
-            if cost[0] in connect or costs[1] in connect:
-                connect.update([cost[0], cost[1]])
+            if cost[0] in routes or cost[1] in routes:
+                routes.update([cost[0], cost[1]])
                 answer += cost[2]
+                costs[i] = [-1, -1, -1]
                 break
     return answer

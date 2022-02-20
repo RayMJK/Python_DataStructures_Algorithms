4b4698c0d054732afed87e05a117196793695b16
RayMJK <mjkraymond93@gmail.com>
2022-02-20 17:42:06 +0900

    2.20

---
+++ Programmers/VVV_정수삼각형.py
@@ -0,0 +1,13 @@
+def solution(triangle):
+    answer = 0
+    #print(triangle) [[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
+    for i in range(1, len(triangle)):
+        for j in range(i+1):
+            if j == 0:
+                triangle[i][j] += triangle[i-1][j]
+            elif j == i:
+                triangle[i][j] += triangle[i-1][j-1]
+            else:
+                triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+    answer = max(triangle[-1])
+    return answer

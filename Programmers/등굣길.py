127c6bbbe208bcba93d32beb0f562dead4d6ea9d
RayMJK <mjkraymond93@gmail.com>
2022-02-28 23:30:53 +0900

    2.28

--- Programmers/등굣길.py
+++ Programmers/등굣길.py
@@ -1,3 +1,7 @@
 def solution(m, n, puddles):
     answer = 0
+    #print(puddles)     [[2, 2]]
+    total = (m-1)*(n-1)
+    p = (puddles[0][0]-1)*(puddles[0][1]-1)+1
+    answer = total - p
     return answer

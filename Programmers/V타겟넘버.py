fe13b270c1f57fb06e963aedc7bf07d1f729c719
RayMJK <mjkraymond93@gmail.com>
2022-03-12 00:03:19 +0900

    3.12

--- Programmers/V타겟넘버.py
+++ Programmers/V타겟넘버.py
@@ -1,10 +1,12 @@
 def solution(numbers, target):
     first = [0]
+    answer = 0
     for i in numbers:
         child = []
         for j in first:
-            child.append(j+i)
             child.append(j-i)
+            child.append(j+i)
         first = child
-        print(first)
-    return first.count(target)
+        #print(first)
+    answer = first.count(target)
+    return answer

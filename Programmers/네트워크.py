094730204144eaaffaae2924341f87bf2478d843
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-03-14 14:25:41 +0900

    Update 네트워크.py

--- Programmers/네트워크.py
+++ Programmers/네트워크.py
@@ -1,4 +1,11 @@
 def solution(n, computers):
     answer = 0
-    print(computers)
-    return answer
+    #print(computers)
+    for i in range(len(computers)):
+        print(i)
+        for j in range(i, n):
+            if j == i:
+                continue
+            if computers[i][j] == 1:
+                answer += 1
+    return n - answer

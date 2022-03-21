4aff40d9b33eddcc1a7168537e462aa851c508f5
RayMJK <mjkraymond93@gmail.com>
2022-03-21 23:12:53 +0900

    3.21

---
+++ Algorithm/Graph/BFS.py
@@ -0,0 +1,13 @@
+def bfs(graph, start_node):
+    visited = list()
+    need_visit = list()
+
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop(0)
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    return visited

772741f10dd08b59f030753ed0c869ddedf717b1
RayMJK <mjkraymond93@gmail.com>
2022-03-21 23:24:16 +0900

    3.21

---
+++ Algorithm/Graph/DFS.py
@@ -0,0 +1,12 @@
+def dfs(graph, start_node):
+    visited = list()
+    need_visit = list()
+    need_visit.append(start_node)
+
+    while need_visit:
+        node = need_visit.pop()
+        if node not in visited:
+            visited.append(node)
+            need_visit.extend(graph[node])
+
+    return visited

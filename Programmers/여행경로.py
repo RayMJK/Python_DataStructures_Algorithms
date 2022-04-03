85372a5b24556b4d3ce82ce81cc9608fbf88c89d
RayMJK <mjkraymond93@gmail.com>
2022-04-04 01:46:01 +0900

    4.4

--- Programmers/여행경로.py
+++ Programmers/여행경로.py
@@ -1,10 +1,14 @@
 def solution(tickets):
     answer = []
+    a = []
     tickets.sort()
     print(tickets)
     for i in range(len(tickets)):
         if tickets[i][0] == 'ICN':
             print(i)
-            answer.append(i)
-    print(answer)
+            a.append(tickets[i][0])
+            a.append(tickets[i][1])
+            break
+    print(a)
+
     return answer

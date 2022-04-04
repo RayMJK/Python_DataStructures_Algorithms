511cfb29c11fc222ec293f75d18da3b4c3dbacd6
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-04-04 17:38:47 +0900

    Update and rename 여행경로.py to VVV_여행경로_DFS.py

---
+++ Programmers/VVV_여행경로_DFS.py
@@ -0,0 +1,29 @@
+def solution(tickets):
+    # 1. 그래프 생성
+    routes = dict()
+
+    for (start, end) in tickets:
+        if routes.get(start) is None:
+            routes[start] = [end]
+        else:
+            routes[start] += [end]
+        print(routes)
+    # 2. 시작점 - [끝점] 역순으로 정렬    
+    for r in routes.keys():
+        routes[r].sort(reverse=True)
+    # print(routes)
+    # 3. DFS 알고리즘으로 path를 만들어줌.
+    st = ["ICN"]
+    path = []
+    
+    while st:
+        top = st[-1]
+        if top not in routes or len(routes[top]) == 0:
+            path.append(st.pop())
+        else:
+            st.append(routes[top][-1])
+            routes[top] = routes[top][:-1]
+            
+    # 4. 만든 path를 거꾸로 돌림.
+    answer = path[::-1]
+    return answer

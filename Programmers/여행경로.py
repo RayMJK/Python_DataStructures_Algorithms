4bcb1b3f34a835b17edc0ea6978d6228c154c037
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2022-04-04 12:03:29 +0900

    Update 여행경로.py

--- Programmers/여행경로.py
+++ Programmers/여행경로.py
@@ -1,14 +1,23 @@
 def solution(tickets):
     answer = []
-    a = []
     tickets.sort()
-    print(tickets)
+    # print(tickets)
     for i in range(len(tickets)):
         if tickets[i][0] == 'ICN':
-            print(i)
-            a.append(tickets[i][0])
-            a.append(tickets[i][1])
+            #print(i)
+            answer.append(tickets[i][0])
+            answer.append(tickets[i][1])
+            tickets.pop(i)
             break
-    print(a)
-
+    # tickets.remove(tickets[i])
+    # print(answer)
+    # print(tickets)
+    while tickets:
+        for i in tickets:
+            # print(i)
+            if i[0] == answer[-1]:
+                answer.append(i[1])
+                tickets.remove(i)
+                break
+    
     return answer

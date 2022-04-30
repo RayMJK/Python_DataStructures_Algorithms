8e54fbb196e8c39e85ba9f0d212a087e4f753b6d
RayMJK <mjkraymond93@gmail.com>
2022-04-30 09:29:11 +0900

    4.30

--- Programmers/V전화번호목록.py
+++ Programmers/V전화번호목록.py
@@ -1,8 +1,9 @@
 def solution(phone_book):
     answer = True
     phone_book.sort()
-    #print(phone_book)
-    for i in range(len(phone_book)-1):
-        if phone_book[i] in phone_book[i+1][:len(phone_book)]:
+    # print(phone_book)
+    for i in range(len(phone_book) - 1):
+
+        if phone_book[i] in phone_book[i + 1][:len(phone_book[i])]:
             return False
     return answer

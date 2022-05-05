d602a70c19036a3d478bc2ff5f3f346a49dcedb9
RayMJK <mjkraymond93@gmail.com>
2022-05-05 23:09:08 +0900

    5.5

--- Programmers/VVV_입국심사_이분탐색.py
+++ Programmers/VVV_입국심사_이분탐색.py
@@ -11,15 +11,15 @@ def solution(n, times):
             # people 은 모든 심사관들이 mid분 동안 심사한 사람의 수
             people += mid // time
             # 모든 심사관을 거치지 않아도 mid분 동안 n명 이상의 심사를 할 수 있다면 반복문을 나간다.
-            if people >= n:
-                break
+            # if people >= n:
+            #     break
 
         # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 많거나 같은 경우
         if people >= n:
-            answer = mid
+
             right = mid - 1
         # 심사한 사람의 수가 심사 받아야할 사람의 수(n)보다 적은 경우
         elif people < n:
             left = mid + 1
-
+    answer = left
     return answer

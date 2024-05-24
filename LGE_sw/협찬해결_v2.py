0b22a4ce3ce84dceb07cdeaa8e2a65decefedee6
Myung Joon Kim <60374051+RayMJK@users.noreply.github.com>
2024-05-24 13:39:40 +0900

    Create 협찬해결_v2.py
    Add 협찬해결 version 2

---
+++ LGE_sw/협찬해결_v2.py
@@ -0,0 +1,18 @@
+import sys
+
+def input_data() :
+	readl = sys.stdin.readline
+	N = int(readl())#협찬 물품의 수
+	D = list(map(int, readl().split()))#선호도 
+	return N, D
+
+
+def Solve():
+	for i in range(1, len(D)) :
+		D[i] = max(D[i], D[i] + D[i-1])
+	return max(D)
+
+#입력받는 부분
+N, D = input_data()
+sol = Solve()
+print(sol)#출력하는 부분

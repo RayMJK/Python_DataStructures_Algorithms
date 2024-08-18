87faba02c978d5f59d257eeaafbb2a3958c837f3
RayMJK <mjkraymond93@gmail.com>
2024-08-19 00:26:36 +0900

    VVV_네트워크_DFS

--- Programmers/VVV_네트워크_DFS.py
+++ Programmers/VVV_네트워크_DFS.py
@@ -1,14 +1,16 @@
-def dfs(x, computers):
-    computers[x][x] = 2  # 들린거 표시
-    for i in range(len(computers[x])):
-        if computers[x][i] == 1 and computers[i][i] != 2:  # 처음 들리는 곳이면
-            dfs(i, computers)
-
-
 def solution(n, computers):
     answer = 0
+    visited = [False for _ in range(n)]
     for i in range(n):
-        if computers[i][i] == 1:  # 처음 들리는 곳
-            dfs(i, computers)
+        if visited[i] == False:
+            dfs(n, computers, i, visited)
             answer += 1
     return answer
+
+def dfs(n, computers, num, visited):
+    visited[num] = True
+    for i in range(n):
+        if i != num and computers[num][i] == 1:
+            if visited[i] == False:
+                dfs(n, computers, i, visited)
+

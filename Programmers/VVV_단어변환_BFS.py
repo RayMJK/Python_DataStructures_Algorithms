46fae13cd9e26cefb137ce57baadd4014f3c8d2b
myungjoon.kim <myungjoon.kim@lge.com>
2024-08-20 16:28:39 +0900

    VVV_단어변환_BFS

--- Programmers/VVV_단어변환_BFS.py
+++ Programmers/VVV_단어변환_BFS.py
@@ -1,26 +1,24 @@
 from collections import deque
 
-
 def solution(begin, target, words):
     answer = 0
+    visited = [False for _ in range(len(words))]
     q = deque()
     q.append([begin, 0])
-    V = [0 for i in range(len(words))]
-    # print(V)
+
     while q:
-        word, cnt = q.popleft()
-        print(word, cnt)
-        if word == target:
-            answer = cnt
+        now_word, step = q.popleft()
+        if now_word == target:
+            answer = step
             break
         for i in range(len(words)):
-            temp_cnt = 0
-            if V[i] == 0:
-                for j in range(len(word)):
-                    if word[j] != words[i][j]:
-                        temp_cnt += 1
-                if temp_cnt == 1:
-                    q.append([words[i], cnt + 1])
-                    V[i] = 1
+            tmp_cnt = 0
+            if visited[i] == False:
+                for j in range(len(now_word)):
+                    if now_word[j] != words[i][j]:
+                        tmp_cnt += 1
+                if tmp_cnt == 1:
+                    q.append([words[i], step+1])
+                    visited[i] = True
 
     return answer

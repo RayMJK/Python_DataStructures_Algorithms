cddc15921e399a490d2a15d2628b504e262e1be5
RayMJK <mjkraymond93@gmail.com>
2024-09-05 23:08:51 +0900

    DFS

---
+++ LGE_sw/VVV_DFS_더하기_DFS.py
@@ -0,0 +1,52 @@
+import sys
+
+def Input_Data():
+    N, K = map(int, readl().split())
+    num = list(map(int, readl().split()))
+    return N, K, num
+
+
+def dfs_binary(n, remain):
+
+    if remain == 0:
+        return True
+    if remain < 0 :
+        return False
+    if n>=N :
+        return False
+    if dfs_binary(n+1, remain-num[n]):
+        return True
+    if dfs_binary(n+1, remain):
+        return True
+
+    return False
+
+def dfs_multi(s, remain):
+
+    if remain == 0:
+        return True
+    if remain < 0 :
+        return False
+    for n in range(s, N):
+        if dfs_multi(n+1, remain-num[n]):
+            return True
+
+sol = []
+# 입력 받는 부분
+readl = sys.stdin.readline
+
+T = int(readl())
+for _ in range(T):
+    N, K, num = Input_Data()
+    # 여기서부터 입력
+    # result = dfs_binary(0, K) # 이진 선택 (중복 순열)
+    result = dfs_multi(0, K) # 조합
+
+    if result == True:
+        sol.append("YES")
+    else:
+        sol.append("NO")
+
+
+# 출력하는 부분
+print(*sol, sep = '\n')

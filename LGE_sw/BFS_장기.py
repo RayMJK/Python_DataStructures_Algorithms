f4d1b55b9cfe1602b0788437745f06d2e7eb8e2f
myungjoon.kim <myungjoon.kim@lge.com>
2024-09-23 17:49:50 +0900

    BFS

--- LGE_sw/BFS_장기.py
+++ LGE_sw/BFS_장기.py
@@ -1,7 +1,5 @@
-from collections import deque
-
 import sys
-
+from collections import deque
 
 def Input_Data():
     readl = sys.stdin.readline
@@ -9,33 +7,35 @@ def Input_Data():
     R, C, S, K = map(int, readl().split())
     return N, M, R, C, S, K
 
+def bfs():
+    visited = [[False] * (M+2) for _ in range(N+2)]
+    directions = [(-2,1), (-2,-1), (-1,2), (-1,-2), (2,1), (2,-1), (1,2), (1,-2)]
+    q = deque()
+    q.append((R,C,0))
+    visited[R][C] = True
+
+    while q:
+        x, y, time = q.popleft()
+        for dx, dy in directions:
+            nx, ny, n_time = x+dx, y+dy, time+1
+            if not 1<=nx<=N:
+                continue
+            if not 1<=ny<=M:
+                continue
+            if visited[nx][ny] == True:
+                continue
+            if nx == S and ny == K:
+                return n_time
+            q.append((nx,ny,n_time))
+            visited[nx][ny] = True
+    return -1
 
 sol = -1
 # 입력 받는 부분
 N, M, R, C, S, K = Input_Data()
 
 # 여기서부터 작성
-directions = [(2,1), (2,-1), (1,2), (1,-2), (-2,1), (-2,-1), (-1,2), (-1,-2)]
-visited = [[False]*(M+2) for _ in range(N+2)]
-def bfs():
-    q = deque()
-    q.append((R,C, 0))
-
-    while q:
-        x, y, time = q.popleft()
-        visited[x][y] = True
-        for dx, dy in directions:
-            nx, ny, ntime = x+dx, y+dy, time+1
-            if nx < 1  or nx > N or ny < 1 or ny > M:
-                continue
-            if visited[nx][ny] == True :
-                continue
-            if nx == S and ny == K:
-                return ntime
-            visited[nx][ny] = True
-            q.append((nx,ny,ntime))
-    return -1
-
 sol = bfs()
+
 # 출력하는 부분
 print(sol)

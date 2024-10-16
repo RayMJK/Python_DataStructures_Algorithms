9056e09a7bb88ba999a81ae2e6357dc764b8c855
myungjoon.kim <myungjoon.kim@lge.com>
2024-10-16 17:49:37 +0900

    DFS

--- LGE_sw/VVV_DFS_건물세우기_DFS.py
+++ LGE_sw/VVV_DFS_건물세우기_DFS.py
@@ -7,6 +7,10 @@ def Input_Data():
     matrix = [[0] + list(map(int, readl().split())) if 1 <= n <= N else [0] * (N + 1) for n in range(N + 1)]
     return N, matrix
 
+"""
+matrix[a][b] : a번 건물을 b번땅에 짓는 비용
+"""
+
 def dfs(n, sum_cost):
     global sol
     if sol <= sum_cost:
@@ -21,6 +25,7 @@ def dfs(n, sum_cost):
         visited[i] = True
         dfs(n+1, sum_cost + matrix[n][i])
         visited[i] = False
+
 def solve():
     global sol
     sol = 100 * N
@@ -34,7 +39,7 @@ N, matrix = Input_Data()
 
 # 여기서부터 작성
 
-visited = [False] * (N + 1)
+visited = [False] * (N+1)
 sol = solve()
 
 # 출력하는 부분

62783c9deaf89f5421005becaf667f9bdefb6e72
RayMJK <mjkraymond93@gmail.com>
2024-11-22 00:23:45 +0900

    이진탐색

--- LGE_sw/이진탐색/V_이진탐색_예산_이진탐색.py
+++ LGE_sw/이진탐색/V_이진탐색_예산_이진탐색.py
@@ -8,29 +8,32 @@ def Input_Data():
     M = int(readl())
     return N, needs, M
 
+
 def check(limit):
-	s = 0
-	for n in needs:
-		if n < limit:
-			s += n
-		else:
-			s += limit
-	if s <= M:
-		return True
-	else:
-		return False
+    s = 0
+    for n in needs:
+        if n < limit:
+            s += n
+        else:
+            s += limit
+    if s <= M:
+        return True
+    else:
+        return False
+
 
 def solve():
-	s = 0
-	e = max(needs)
-	while s<=e:
-		m = (s+e)//2
-		if check(m):
-			sol = m
-			s = m+1
-		else:
-			e = m-1
-	return sol
+    s = 0
+    e = max(needs)
+    while s <= e:
+        m = (s + e) // 2
+        if check(m):
+            sol = m
+            s = m + 1
+        else:
+            e = m - 1
+    return sol
+
 
 sol = -1
 # 입력받는 부분

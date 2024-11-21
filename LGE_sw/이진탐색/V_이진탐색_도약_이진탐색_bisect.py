08d0b4a27ef8ce4e3a4446af13bd70b3fd0d1b84
RayMJK <mjkraymond93@gmail.com>
2024-11-21 23:39:09 +0900

    이진탐색

---
+++ LGE_sw/이진탐색/V_이진탐색_도약_이진탐색_bisect.py
@@ -0,0 +1,34 @@
+import sys
+import bisect
+
+def Input_Data():
+	readl = sys.stdin.readline
+	N = int(readl())
+	pos = [int(readl()) for _ in range(N)]
+	return N, pos
+
+def solve():
+	pos.sort()
+	cnt = 0
+	for s1 in range(N-2):
+		for s2 in range(s1+1, N-1):
+			jump = pos[s2] - pos[s1]
+			rs = pos[s2] + jump
+			re = pos[s2] + 2*jump
+			lower = bisect.bisect_left(pos, rs)
+			if lower == N or pos[lower] > re:
+				continue
+			upper = bisect.bisect_right(pos, re)
+			cnt += upper-lower
+	return cnt
+
+
+sol = -1
+# 입력받는 부분
+N, pos = Input_Data()
+
+# 여기서부터 작성
+sol = solve()
+
+# 출력하는 부분
+print(sol)
